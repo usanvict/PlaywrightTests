2afdf3067b45dc22738866fec1a2c17be35af21c
Victoria Usan <victoria.usan@gmail.com>
2025-01-17 12:07:28 +0100

    Kofio update

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,24 +10,46 @@ namespace PlaywrightTests;
 public class Tests : PageTest
 {
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
+        //If I'm not happy with timeouts, I can change it here
+        // Page.SetDefaultTimeout(3000);
+        await Page.GotoAsync("https://kofio.cz", new PageGotoOptions
+        {
+            WaitUntil = WaitUntilState.NetworkIdle
+        });
     }
 
     [Test]
     public async Task HasTitle()
     {
-        await Page.GotoAsync("https://playwright.dev");
-
         // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+        await Expect(Page).ToHaveTitleAsync(new Regex("Kofio"));
+        //For a strcit equation, remove Regex
     }
 
     [Test]
+    public async Task HasCoffee()
+    {
+        var coffeeButton = Page.Locator("#navbar").GetByRole(AriaRole.Link, new() { Name = "Káva" });
+        await Expect(coffeeButton).ToBeVisibleAsync();
+        await coffeeButton.ClickAsync();
+        await Expect(Page.Locator("#filterTab")).ToHaveTextAsync("Káva");
+    }
+
+    [Test]
+    public async Task LookForHAYB()
+    {
+        var searchFiled = Page.Locator("#front_search_inp_lg");
+        await searchFiled.PressSequentiallyAsync("HAYB");
+        await Expect(Page.Locator("#search_complete_results")).ToBeVisibleAsync();
+        await Expect(Page.Locator("//h5[contains(text(), 'Pražírny')]//..//mark[contains(text(), 'HAYB')]")).ToBeVisibleAsync();
+    }
+
+
+    [Skip]
     public async Task GetStartedLink()
     {
-        await Page.GotoAsync("https://playwright.dev");
-
         // Click the get started link.
         await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
 
